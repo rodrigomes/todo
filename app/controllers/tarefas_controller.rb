d9b089cc14f220df3afd6300addf808a58c38526
elogroup <rgomes.rodrigo@gmail.com>
2012-07-12 17:18:53 -0300

    rails generate rspec:install

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -4,10 +4,14 @@ class TarefasController < ApplicationController
 		@tarefa = Tarefa.new
 	end
 
+	def index
+		@tarefas = Tarefa.all
+	end
+
 	def create
 		@tarefa = Tarefa.new(params[:tarefa])
 		unless @tarefa.save 
-			redirect_to :action => new
+			redirect_to new_tarefa_path
 		end
 	end
 
