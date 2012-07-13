a5f07615558bc76a177fbcac782b1933eea45fe1
elogroup <rgomes.rodrigo@gmail.com>
2012-07-13 11:19:40 -0300

    deletamos o public index e adicionamos o root to tarefas#index

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Todo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'tarefas#index'
 
   # See how all your routes lay out with "rake routes"
 
