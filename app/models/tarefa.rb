48cff8fc4d59d0a832f1367ab77cb1ce30b4bc48
elogroup <rgomes.rodrigo@gmail.com>
2012-07-13 13:13:28 -0300

    adicionando o link para o projeto no index de tarefas E o projeto1 no form de new tarefa

--- app/models/tarefa.rb
+++ app/models/tarefa.rb
@@ -1,5 +1,5 @@
 class Tarefa < ActiveRecord::Base
-  attr_accessible :data_fim, :descricao, :responsavel, :nome
+  attr_accessible :data_fim, :descricao, :responsavel, :nome, :projeto_id
 
   validates_presence_of :responsavel
 
