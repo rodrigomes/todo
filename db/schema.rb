2e63b8525b96bc5c74c816d04a5276ca01f80671
elogroup <rgomes.rodrigo@gmail.com>
2012-07-13 11:25:06 -0300

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120712205351) do
+ActiveRecord::Schema.define(:version => 20120713142417) do
+
+  create_table "projetos", :force => true do |t|
+    t.string   "nome"
+    t.date     "data_inicio"
+    t.date     "data_fim"
+    t.string   "cliente"
+    t.string   "descricao"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "tarefas", :force => true do |t|
     t.string   "descricao"
